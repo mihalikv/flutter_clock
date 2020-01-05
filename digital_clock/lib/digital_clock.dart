03644b400e11a6277a2ae44078c28bd3e65df9f5
Viliam Mihalik <viliam.mihalik@smartbase.sk>
2020-01-05 15:36:54 +0100

    digital clock test

--- digital_clock/lib/digital_clock.dart
+++ digital_clock/lib/digital_clock.dart
@@ -4,26 +4,40 @@
 
 import 'dart:async';
 
+import 'package:flare_flutter/flare.dart';
+import 'package:flare_flutter/flare_controller.dart';
 import 'package:flutter_clock_helper/model.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flare_dart/math/mat2d.dart';
 
-enum _Element {
-  background,
-  text,
-  shadow,
-}
+enum _Element { background, text, shadow, animationAsset }
 
 final _lightTheme = {
-  _Element.background: Color(0xFF81B3FE),
-  _Element.text: Colors.white,
+  _Element.background: Colors.white,
+  _Element.text: Colors.black,
   _Element.shadow: Colors.black,
+  _Element.animationAsset: "assets/export.flr",
 };
 
 final _darkTheme = {
   _Element.background: Colors.black,
   _Element.text: Colors.white,
   _Element.shadow: Color(0xFF174EA6),
+};
+
+final _numberAnimationOrder = {
+  0: "zeroToOne",
+  1: "oneToTwo",
+//  2: "twoToThree",
+//  3: "threeToFour",
+//  4: "fourToFive",
+//  5: "fiveToSix",
+//  6: "sixToSeven",
+//  7: "sevenToEight",
+//  8: "eightToNine",
+//  9: "nineToZero",
 };
 
 /// A basic digital clock.
@@ -38,9 +52,51 @@ class DigitalClock extends StatefulWidget {
   _DigitalClockState createState() => _DigitalClockState();
 }
 
-class _DigitalClockState extends State<DigitalClock> {
+class _DigitalClockState extends State<DigitalClock> with FlareController {
   DateTime _dateTime = DateTime.now();
   Timer _timer;
+  double _digitTime = 0.0;
+  double _animationSpeedRatio = 1;
+  double stopTime = 2;
+  int _counter = 0;
+
+  final List<FlareAnimationLayer> _digitAnimations = [];
+  ActorAnimation _digit;
+  FlareAnimationLayer current_layer;
+
+  @override
+  void initialize(FlutterActorArtboard artboard) {
+    _counter = 0;
+    for (var entry in _numberAnimationOrder.entries) {
+      _digitAnimations.add(FlareAnimationLayer()..animation = artboard.getAnimation(entry.value));
+    }
+    current_layer = _digitAnimations[0];
+  }
+
+  @override
+  void setViewTransform(Mat2D viewTransform) {}
+
+  @override
+  bool advance(FlutterActorArtboard artboard, double elapsed) {
+    if (stopTime > 0) {
+      stopTime -= elapsed;
+      return true;
+    }
+    _digitTime += elapsed * _animationSpeedRatio;
+    current_layer.time = _digitTime;
+    current_layer.mix = 1;
+    if (current_layer.isDone) {
+      current_layer.apply(artboard);
+      _counter = (_counter + 1) % _digitAnimations.length;
+      current_layer = _digitAnimations[_counter];
+      _digitTime = 0;
+      stopTime = 2;
+      return true;
+    }
+
+    current_layer.apply(artboard);
+    return true;
+  }
 
   @override
   void initState() {
@@ -78,18 +134,18 @@ class _DigitalClockState extends State<DigitalClock> {
       _dateTime = DateTime.now();
       // Update once per minute. If you want to update every second, use the
       // following code.
-      _timer = Timer(
-        Duration(minutes: 1) -
-            Duration(seconds: _dateTime.second) -
-            Duration(milliseconds: _dateTime.millisecond),
-        _updateTime,
-      );
+//      _timer = Timer(
+//        Duration(minutes: 1) -
+//            Duration(seconds: _dateTime.second) -
+//            Duration(milliseconds: _dateTime.millisecond),
+//        _updateTime,
+//      );
       // Update once per second, but make sure to do it at the beginning of each
       // new second, so that the clock is accurate.
-      // _timer = Timer(
-      //   Duration(seconds: 1) - Duration(milliseconds: _dateTime.millisecond),
-      //   _updateTime,
-      // );
+      _timer = Timer(
+        Duration(seconds: 1) - Duration(milliseconds: _dateTime.millisecond),
+        _updateTime,
+      );
     });
   }
 
@@ -101,34 +157,45 @@ class _DigitalClockState extends State<DigitalClock> {
     final hour =
         DateFormat(widget.model.is24HourFormat ? 'HH' : 'hh').format(_dateTime);
     final minute = DateFormat('mm').format(_dateTime);
-    final fontSize = MediaQuery.of(context).size.width / 3.5;
-    final offset = -fontSize / 7;
-    final defaultStyle = TextStyle(
-      color: colors[_Element.text],
-      fontFamily: 'PressStart2P',
-      fontSize: fontSize,
-      shadows: [
-        Shadow(
-          blurRadius: 0,
-          color: colors[_Element.shadow],
-          offset: Offset(10, 0),
-        ),
-      ],
-    );
 
     return Container(
       color: colors[_Element.background],
-      child: Center(
-        child: DefaultTextStyle(
-          style: defaultStyle,
-          child: Stack(
-            children: <Widget>[
-              Positioned(left: offset, top: 0, child: Text(hour)),
-              Positioned(right: offset, bottom: offset, child: Text(minute)),
-            ],
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: <
+          Widget>[
+        Expanded(
+          child: FlareActor(
+            colors[_Element.animationAsset],
+            controller: this,
+            snapToEnd: true,
           ),
         ),
-      ),
+//        Expanded(
+//          child: FlareActor(colors[_Element.animationAsset], controller: this),
+//        ),
+//        Expanded(
+//            child: Row(
+//          children: <Widget>[
+//            Expanded(
+//              child:
+//                  FlareActor(colors[_Element.animationAsset], controller: this),
+//            ),
+//            Expanded(
+//              child:
+//                  FlareActor(colors[_Element.animationAsset], controller: this),
+//            ),
+//          ],
+//          mainAxisAlignment: MainAxisAlignment.center,
+//        )),
+//        Expanded(
+//          child: FlareActor(colors[_Element.animationAsset], controller: this),
+//        ),
+//        Expanded(
+//          child: FlareActor(
+//            colors[_Element.animationAsset],
+//            animation: "zeroToOne",
+//          ),
+//        )
+      ]),
     );
   }
 }
