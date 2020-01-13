0f3eecf95a5d64737af812c754719b5149d737c5
Viliam Mihalik <viliam.mihalik@smartbase.sk>
2020-01-13 23:50:29 +0100

    digital clock test

--- digital_clock/lib/digital_clock.dart
+++ digital_clock/lib/digital_clock.dart
@@ -4,9 +4,13 @@
 
 import 'dart:async';
 
+import 'package:flare_flutter/flare.dart';
+import 'package:flare_flutter/flare_controller.dart';
 import 'package:flutter_clock_helper/model.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flare_dart/math/mat2d.dart';
 
 enum _Element {
   background,
@@ -38,9 +42,81 @@ class DigitalClock extends StatefulWidget {
   _DigitalClockState createState() => _DigitalClockState();
 }
 
+class TickController extends FlareController {
+  ActorAnimation _element;
+  String _animationName;
+
+  TickController(String animationName) {
+    this._animationName = animationName;
+  }
+
+  @override
+  void initialize(FlutterActorArtboard artboard) {
+    _element = artboard.getAnimation(this._animationName);
+  }
+
+  @override
+  bool advance(FlutterActorArtboard artboard, double elapsed) {
+    final animationTime =
+        ((DateTime
+            .now()
+            .second * 1000) + DateTime
+            .now()
+            .millisecond) / 1000;
+    _element.apply(animationTime, artboard, 1);
+    return true;
+  }
+
+  @override
+  void setViewTransform(Mat2D viewTransform) {}
+}
+
+class MinuteController extends TickController {
+  MinuteController(String animationName) : super(animationName);
+
+  ActorAnimation _elementSecondary;
+  double animationTimeSecondary = 0;
+
+  @override
+  void initialize(FlutterActorArtboard artboard) {
+    _element = artboard.getAnimation(this._animationName);
+    _elementSecondary = artboard.getAnimation('Wind');
+  }
+
+  @override
+  bool advance(FlutterActorArtboard artboard, double elapsed) {
+    final animationTime = DateTime
+        .now()
+        .minute * 60.0;
+    _element.apply(animationTime, artboard, 1);
+    animationTimeSecondary += elapsed;
+    animationTimeSecondary =
+        animationTimeSecondary % _elementSecondary.duration;
+    _elementSecondary.apply(animationTimeSecondary, artboard, 1);
+    return true;
+  }
+}
+
+class HourController extends TickController {
+  HourController(String animationName) : super(animationName);
+
+  @override
+  bool advance(FlutterActorArtboard artboard, double elapsed) {
+    final animationTime = ((DateTime
+        .now()
+        .hour % 12) * 60.0 * 60.0) +
+        (DateTime
+            .now()
+            .minute * 60.0);
+    _element.apply(animationTime, artboard, 1);
+    return true;
+  }
+}
+
 class _DigitalClockState extends State<DigitalClock> {
   DateTime _dateTime = DateTime.now();
   Timer _timer;
+  AssetImage _bgImage;
 
   @override
   void initState() {
@@ -70,6 +146,11 @@ class _DigitalClockState extends State<DigitalClock> {
   void _updateModel() {
     setState(() {
       // Cause the clock to rebuild when the model changes.
+      if (widget.model.weatherCondition == WeatherCondition.sunny) {
+        this._bgImage = AssetImage('assets/bg_summer.png');
+      } else {
+        this._bgImage = AssetImage('assets/bg_winter.png');
+      }
     });
   }
 
@@ -78,57 +159,60 @@ class _DigitalClockState extends State<DigitalClock> {
       _dateTime = DateTime.now();
       // Update once per minute. If you want to update every second, use the
       // following code.
-      _timer = Timer(
-        Duration(minutes: 1) -
-            Duration(seconds: _dateTime.second) -
-            Duration(milliseconds: _dateTime.millisecond),
-        _updateTime,
-      );
+//      _timer = Timer(
+//        Duration(minutes: 1) -
+//            Duration(seconds: _dateTime.second) -
+//            Duration(milliseconds: _dateTime.millisecond),
+//        _updateTime,
+//      );
       // Update once per second, but make sure to do it at the beginning of each
       // new second, so that the clock is accurate.
-      // _timer = Timer(
-      //   Duration(seconds: 1) - Duration(milliseconds: _dateTime.millisecond),
-      //   _updateTime,
-      // );
+      _timer = Timer(
+        Duration(seconds: 1) - Duration(milliseconds: _dateTime.millisecond),
+        _updateTime,
+      );
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    final colors = Theme.of(context).brightness == Brightness.light
+    final colors = Theme
+        .of(context)
+        .brightness == Brightness.light
         ? _lightTheme
         : _darkTheme;
-    final hour =
-        DateFormat(widget.model.is24HourFormat ? 'HH' : 'hh').format(_dateTime);
-    final minute = DateFormat('mm').format(_dateTime);
-    final fontSize = MediaQuery.of(context).size.width / 3.5;
-    final offset = -fontSize / 7;
-    final defaultStyle = TextStyle(
-      color: colors[_Element.text],
-      fontFamily: 'PressStart2P',
-      fontSize: fontSize,
-      shadows: [
-        Shadow(
-          blurRadius: 0,
-          color: colors[_Element.shadow],
-          offset: Offset(10, 0),
-        ),
-      ],
-    );
 
     return Container(
-      color: colors[_Element.background],
-      child: Center(
-        child: DefaultTextStyle(
-          style: defaultStyle,
-          child: Stack(
-            children: <Widget>[
-              Positioned(left: offset, top: 0, child: Text(hour)),
-              Positioned(right: offset, bottom: offset, child: Text(minute)),
-            ],
-          ),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: _bgImage,
+          fit: BoxFit.contain,
         ),
       ),
+      child: Stack(children: <Widget>[
+        FlareActor("assets/Cloud.flr",
+            alignment: Alignment.topLeft,
+            fit: BoxFit.contain,
+            animation: "Main"),
+        Container(
+          alignment: Alignment.center,
+          child: Image.asset("assets/face.png"),
+        ),
+        FlareActor("assets/Second.flr",
+            alignment: Alignment.center,
+            fit: BoxFit.contain,
+            controller: TickController("Main")),
+        FlareActor("assets/Minute.flr",
+            alignment: Alignment.center,
+            fit: BoxFit.contain,
+            controller: MinuteController("Main")),
+        FlareActor("assets/Hour.flr",
+            alignment: Alignment.center,
+            fit: BoxFit.contain,
+            controller: HourController("Main")),
+
+
+      ]),
     );
   }
 }
